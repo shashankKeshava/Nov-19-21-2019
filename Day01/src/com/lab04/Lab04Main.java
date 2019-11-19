f02655ea95594cfb06f2b21aadc4059750a220b4
prabhu-durasoft <iamprabhu@yahoo.com>
2019-11-19 14:20:45 +0530

    Day01

--- Day01/src/com/lab04/Lab04Main.java
+++ Day01/src/com/lab04/Lab04Main.java
@@ -1,10 +1,27 @@
 package com.lab04;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Lab04Main {
 
 	public static void main(String[] args) {
 		String input = "+5 -1 +9 +5 -7 +5 -3 +2 -4 +6 +8";
 		
+		oldStyle(input);
+		java8Style(input);
+	}
+
+	private static void java8Style(String input) {
+		int total = Arrays
+						.asList(input.split(" "))
+						.stream()
+						.mapToInt(e -> Integer.parseInt(e))
+						.sum();	
+		System.out.println("Total: " + total);
+	}
+
+	private static void oldStyle(String input) {
 		String[] items = input.split(" ");
 		int total = 0;
 		for (String item : items) {
@@ -12,7 +29,6 @@ public class Lab04Main {
 			total += num;
 		}
 		System.out.println("Total: " + total);
-		
 	}
 
 }
