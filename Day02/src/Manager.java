bf848ec0a5f3b6e7e7f26deadb8ddf4de53d82ec
prabhu-durasoft <iamprabhu@yahoo.com>
2019-11-20 11:18:52 +0530

    Day02

---
+++ Day02/src/Manager.java
@@ -0,0 +1,26 @@
+//Manager is an Employee
+//derived class
+//sub class
+public class Manager extends Employee {
+	private int stockOptions;
+
+	//Derived class should call
+	//the base class constructor
+	public Manager(String name,
+			double salary,
+			int stockOptions) {
+		super(name, salary);		
+		this.stockOptions = stockOptions;
+	}
+
+	//Method Overriding
+	public void work(int hours) {
+		//super.work(hours);
+		System.out.println("Manager: " + getName() + " works for " + hours + " hours");
+	}
+	
+}
+
+//class Donkey extends Employee {
+//	
+//}
